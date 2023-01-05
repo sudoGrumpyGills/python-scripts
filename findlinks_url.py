82c0b6b815c164bdc4e54e7aa4e35e2dc6a46505
Josh Wallace <102870654+sudoGrumpyGills@users.noreply.github.com>
2023-01-05 10:58:18 -0500

    Update findlinks_url.py

--- findlinks_url.py
+++ findlinks_url.py
@@ -8,6 +8,8 @@ soup = BeautifulSoup(reqs.text, 'html.parser')
 
 for link in soup.find_all('a'):
 	domain = urlparse(link.get('href')).netloc
+	# print the full link if it is a bit.ly link, use case had bit.ly links but it would be a good idea to add checks for other URL shortners
+	# in the use case, I was looking to whitelist links on a webpage, and I did not want to whitelist the domain for a URL shortener
 	if "bit.ly" in domain:
 		print(f"{urlparse(link.get('href')).netloc},{urlparse(link.get('href')).path}")
 	elif domain.isspace() or (domain == ""):

f9bcc8eed06b49781c8844117ae3608cbb83f571
Josh Wallace <102870654+sudoGrumpyGills@users.noreply.github.com>
2023-01-05 14:01:53 -0500

    Update findlinks_url.py

--- findlinks_url.py
+++ findlinks_url.py
@@ -1,18 +1,21 @@
+#script to pull links from a webpage, and print a list of the unique domains, this is for whitelisting requests
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 #set the URL to get links from below
-url = 'https://kb.joshtheadmin.com'
+url = 'https://www.assaabloydooraccessories.us/en/resource-library/catalogs-brochures/'
 reqs = requests.get(url)
 soup = BeautifulSoup(reqs.text, 'html.parser')
-
+url_list = []
 for link in soup.find_all('a'):
 	domain = urlparse(link.get('href')).netloc
-	# print the full link if it is a bit.ly link, use case had bit.ly links but it would be a good idea to add checks for other URL shortners
-	# in the use case, I was looking to whitelist links on a webpage, and I did not want to whitelist the domain for a URL shortener
 	if "bit.ly" in domain:
-		print(f"{urlparse(link.get('href')).netloc}{urlparse(link.get('href')).path}")
+		#print(f"{urlparse(link.get('href')).netloc}{urlparse(link.get('href')).path}")
+		url_list.append(f"{urlparse(link.get('href')).netloc}{urlparse(link.get('href')).path}")
 	elif domain.isspace() or (domain == ""):
 		continue
 	else:
-		print(domain)
+		#print(domain)
+		url_list.append(domain)
+for domain in set(url_list):
+	print (domain)
